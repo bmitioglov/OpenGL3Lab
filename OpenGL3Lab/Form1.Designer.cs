8fcc463ad4d9ad1b321e3966e5859f79e62b5fc5
Mitioglov Boris <ibmttz13.5@gmail.com>
2013-04-05 02:25:25 +0400

    1

---
+++ OpenGL3Lab/Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace OpenGL3Lab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AnT = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.trackBar4 = new System.Windows.Forms.TrackBar();
+            this.trackBar5 = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.RenderTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AnT
+            // 
+            this.AnT.AccumBits = ((byte)(0));
+            this.AnT.AutoCheckErrors = false;
+            this.AnT.AutoFinish = false;
+            this.AnT.AutoMakeCurrent = true;
+            this.AnT.AutoSwapBuffers = true;
+            this.AnT.BackColor = System.Drawing.Color.Black;
+            this.AnT.ColorBits = ((byte)(32));
+            this.AnT.DepthBits = ((byte)(16));
+            this.AnT.Location = new System.Drawing.Point(12, 12);
+            this.AnT.Name = "AnT";
+            this.AnT.Size = new System.Drawing.Size(377, 336);
+            this.AnT.StencilBits = ((byte)(0));
+            this.AnT.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(519, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "По оси";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Вращать вдоль X",
+            "Вращать вдоль Y",
+            "Вращать вдоль Z"});
+            this.comboBox1.Location = new System.Drawing.Point(414, 29);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(268, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(462, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Объект для визуализации";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Сфера",
+            "Цилиндр",
+            "Куб ",
+            "Конус ",
+            "Тор"});
+            this.comboBox2.Location = new System.Drawing.Point(414, 85);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(268, 21);
+            this.comboBox2.TabIndex = 4;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(414, 125);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(75, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Сеточный";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged_1);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(414, 169);
+            this.trackBar1.Maximum = 50000;
+            this.trackBar1.Minimum = -50000;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(45, 159);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(414, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "X";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(465, 169);
+            this.trackBar2.Maximum = 50000;
+            this.trackBar2.Minimum = -50000;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar2.Size = new System.Drawing.Size(45, 159);
+            this.trackBar2.TabIndex = 8;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(578, 169);
+            this.trackBar3.Maximum = 360;
+            this.trackBar3.Minimum = -360;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar3.Size = new System.Drawing.Size(45, 159);
+            this.trackBar3.TabIndex = 9;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // trackBar4
+            // 
+            this.trackBar4.Location = new System.Drawing.Point(516, 169);
+            this.trackBar4.Maximum = 50000;
+            this.trackBar4.Minimum = -50000;
+            this.trackBar4.Name = "trackBar4";
+            this.trackBar4.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar4.Size = new System.Drawing.Size(45, 159);
+            this.trackBar4.TabIndex = 10;
+            this.trackBar4.Scroll += new System.EventHandler(this.trackBar4_Scroll);
+            // 
+            // trackBar5
+            // 
+            this.trackBar5.Location = new System.Drawing.Point(637, 169);
+            this.trackBar5.Maximum = 5000;
+            this.trackBar5.Minimum = -5000;
+            this.trackBar5.Name = "trackBar5";
+            this.trackBar5.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar5.Size = new System.Drawing.Size(45, 159);
+            this.trackBar5.TabIndex = 11;
+            this.trackBar5.Scroll += new System.EventHandler(this.trackBar5_Scroll);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(465, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Y";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(516, 149);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(14, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Z";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(578, 149);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Angle";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(637, 149);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Zoom";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(414, 334);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(13, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "0";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(462, 333);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(13, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "0";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(516, 333);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(13, 13);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "0";
+            // 
+            // RenderTimer
+            // 
+            this.RenderTimer.Interval = 30;
+            this.RenderTimer.Tick += new System.EventHandler(this.RenderTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(694, 360);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.trackBar5);
+            this.Controls.Add(this.trackBar4);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.AnT);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl AnT;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.TrackBar trackBar4;
+        private System.Windows.Forms.TrackBar trackBar5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Timer RenderTimer;
+    }
+}
+
---
+++ OpenGL3Lab/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// для работы с библиотекой OpenGL 
+using Tao.OpenGl;
+// для работы с библиотекой FreeGLUT 
+using Tao.FreeGlut;
+// для работы с элементом управления SimpleOpenGLControl 
+using Tao.Platform.Windows;
+
+namespace OpenGL3Lab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        // вспомогательные переменные - в них будут хранится обработанные значения, 
+        // полученные при перетаскивании ползунков пользователем 
+        double a = 0, b = 0, c = -5, d = 0, zoom = 1; // выбранные оси 
+        int os_x = 1, os_y = 0, os_z = 0;
+
+        // режим сеточной визуализации 
+        bool Wire = false;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AnT.InitializeContexts();
+            // инициализация бибилиотеки glut 
+            Glut.glutInit();
+            // инициализация режима экрана 
+            Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE);
+
+            // установка цвета очистки экрана (RGBA) 
+            Gl.glClearColor(255, 255, 255, 1);
+
+            // установка порта вывода 
+            Gl.glViewport(0, 0, AnT.Width, AnT.Height);
+
+            // активация проекционной матрицы 
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            // очистка матрицы 
+            Gl.glLoadIdentity();
+
+            // установка перспективы 
+            Glu.gluPerspective(45, (float)AnT.Width / (float)AnT.Height, 0.1, 200);
+
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+            Gl.glLoadIdentity();
+
+            // начальная настройка параметров openGL (тест глубины, освещение и первый источник света) 
+            Gl.glEnable(Gl.GL_DEPTH_TEST);
+            Gl.glEnable(Gl.GL_LIGHTING);
+            Gl.glEnable(Gl.GL_LIGHT0);
+
+            // установка первых элементов в списках combobox 
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+            
+            
+            // активация таймера, вызывающего функцию для визуализации 
+            RenderTimer.Start();
+        }
+
+        private void RenderTimer_Tick(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+      
+
+
+
+       
+        // функция отрисовки 
+        private void Draw()
+        {
+            // очистка буфера цвета и буфера глубины 
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+
+            Gl.glClearColor(255, 255, 255, 1);
+            // очищение текущей матрицы 
+            Gl.glLoadIdentity();
+
+            // помещаем состояние матрицы в стек матриц , дальнейшие трансформации затронут только визуализацию объекта 
+            Gl.glPushMatrix();
+            // производим перемещение, в зависимости от значений, полученных при перемещении ползунков 
+            Gl.glTranslated(a, b, c);
+            // поворот по установленной оси 
+            Gl.glRotated(d, os_x, os_y, os_z);
+            // и масштабирование объекта 
+            Gl.glScaled(zoom, zoom, zoom);
+
+            // в зависимсоти от установленного типа объекта 
+            switch (comboBox2.SelectedIndex)
+            {
+                // рисуем нужный объект, использую фунции бибилиотеки GLUT 
+                case 0:
+                    {
+                        if (Wire) // если установлен сеточный режим визуализации 
+                            Glut.glutWireSphere(2, 16, 16); // сеточная сфера 
+                        else
+                            Glut.glutSolidSphere(2, 16, 16); // полигональная сфера 
+                        break;
+                    }
+                case 1:
+                    {
+                        if (Wire) // если установлен сеточный режим визуализации 
+                            Glut.glutWireCylinder(1, 2, 32, 32); // цилиндр 
+                        else
+                            Glut.glutSolidCylinder(1, 2, 32, 32);
+                        break;
+                    }
+                case 2:
+                    {
+                        if (Wire) // если установлен сеточный режим визуализации 
+                            Glut.glutWireCube(2); // куб 
+                        else
+                            Glut.glutSolidCube(2);
+                        break;
+                    }
+                case 3:
+                    {
+                        if (Wire) // если установлен сеточный режим визуализации 
+                            Glut.glutWireCone(2, 3, 32, 32); // конус 
+                        else
+                            Glut.glutSolidCone(2, 3, 32, 32);
+                        break;
+                    }
+                case 4:
+                    {
+                        if (Wire) // если установлен сеточный режим визуализации 
+                            Glut.glutWireTorus(0.2, 2.2, 32, 32); // тор 
+                        else
+                            Glut.glutSolidTorus(0.2, 2.2, 32, 32);
+                        break;
+                    }
+            }
+
+            // возвращаем состояние матрицы 
+            Gl.glPopMatrix();
+
+            // завершаем рисование 
+            Gl.glFlush();
+
+            // обновлем элемент AnT 
+            AnT.Invalidate();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            // переводим значение, установившееся в элементе trackBar в необходимый нам формат 
+            a = (double)trackBar1.Value / 1000.0;
+            // подписываем это значение в label элементе под данным ползунком 
+            label8.Text = a.ToString();
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            // переводим значение, установившееся в элементе trackBar в необходимый нам формат 
+            b = (double)trackBar2.Value / 1000.0;
+            // подписываем это значение в label элементе под данным ползунком 
+            label9.Text = b.ToString();
+        }
+
+        private void trackBar4_Scroll(object sender, EventArgs e)
+        {
+            // переводим значение, установившееся в элементе trackBar в необходимый нам формат 
+            c = (double)trackBar4.Value / 1000.0;
+            // подписываем это значение в label элементе под данным ползунком 
+            label10.Text = c.ToString();
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            // переводим значение, установившееся в элементе trackBar в необходимый нам формат 
+            d = (double)trackBar3.Value;
+            // подписываем это значение в label элементе под данным ползунком 
+            //label6.Text = d.ToString();
+        }
+
+        private void trackBar5_Scroll(object sender, EventArgs e)
+        {
+            // переводим значение, установившееся в элементе trackBar в необходимый нам формат 
+            zoom = (double)trackBar5.Value / 1000.0;
+            // подписываем это значение в label элементе под данным ползунком 
+            //label6.Text = zoom.ToString();
+        }
+
+        private void checkBox1_CheckedChanged_1(object sender, EventArgs e)
+        {
+            // если отмечен 
+            if (checkBox1.Checked)
+            {
+                // устанавливаем сеточный режим визуализации 
+                Wire = true;
+            }
+            else
+            {
+                // иначе - полигональная визуализация 
+                Wire = false;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                // устанавливаем необходимую ось (будет испльзовано в функции glRotate**) 
+                case 0:
+                    {
+                        os_x = 1;
+                        os_y = 0;
+                        os_z = 0;
+                        break;
+                    }
+                case 1:
+                    {
+                        os_x = 0;
+                        os_y = 1;
+                        os_z = 0;
+                        break;
+                    }
+                case 2:
+                    {
+                        os_x = 0;
+                        os_y = 0;
+                        os_z = 1;
+                        break;
+                    }
+            }
+        }
+
+
+        
+    }
+}
